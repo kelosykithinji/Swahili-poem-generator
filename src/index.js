6f0a531c4fb06bb85b017f4177fbfc84cd8a3a1f
kelosy kithinji <kelosykithinji@gmail.com>
2024-12-30 09:44:03 +0400

    added basic javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-//alert("hello world!");
+function generatePoem(event){
+    event.preventDefault();
+    
+    //the typewriter is responsible for the typing style
+    new Typewriter("#poem", {
+      strings: "Hapa ndipo umenifikisha",
+      autoStart: true,
+      delay:1,
+      cursor:"",
+    });
+
+}
+
+let formContentElement = document.querySelector("#form-content");
+formContentElement.addEventListener("submit", generatePoem);
